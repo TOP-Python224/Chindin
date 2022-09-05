09b72445d790f76428c3ed94bc58512fd4edc14f
Chindin Grigoriy <111311447+lookLikeGrey@users.noreply.github.com>
2022-09-05 22:29:27 +0300

    Add files via upload

---
+++ 07.06/1.1.py
@@ -0,0 +1,12 @@
+import datetime as dt
+
+now_year = dt.datetime.now().year
+
+print("Введите имя: ")
+name=input()
+print("Введите фамилию: ")
+surname=input()
+print("Введите год рождения: ")
+birthday=input()
+
+print(name, surname, ',', now_year-int(birthday))
